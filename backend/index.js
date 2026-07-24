49f1e08d55629298a2e5f7bca2cb1bc77aae2e58
Kunal Giri <kunalgiri177@gmail.com>
2026-07-24 17:45:03 +0530

    feat: set up Express server with CORS and cookie-parser middleware

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,20 @@
 import dns from "dns";
 dns.setServers(['8.8.8.8', '1.1.1.1']);
-import express from "express"
+
 import dotenv from "dotenv"
 import connectDb from "./config/db.js";
-import cookieParser from "cookie-parser";
-import authRouter from "./route/authRoute.js";
+import app from "./app.js";
+
 dotenv.config();
-const port = process.env.PORT
 
-const app = express()
-app.use(cookieParser())
-app.use(express.json())
+const port = process.env.PORT || 8000;
 
-
-
-app.use("/api/auth", authRouter)
-
-
-
-app.listen(port, () => {
-    console.log("Server Started")
-    connectDb();
+connectDb()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`⚙️ Server is running at port : ${port}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
 })
