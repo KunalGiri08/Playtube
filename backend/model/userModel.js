8622f42c2cd52c068463d2ad097537a8bdbbb4fa
Kunal Giri <kunalgiri177@gmail.com>
2026-07-27 19:09:30 +0530

    feat: add OTP reset functionality and nodemailer dependency for email verification

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,30 +1,42 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
+    username: {
+        type: String,
         required: true
     },
-    photoUrl:{
-        type:String,
-        default:""
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    channel:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Channel"
-    }
+    password: {
+        type: String,
+        required: true
+    },
+    photoUrl: {
+        type: String,
+        default: ""
+    },
+    channel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel"
+    },
+     // 🔐 OTP reset system
+  resetOtp: {
+        type: String
+    },
+    otpExpires: {
+        type: Date
+    },
+    isOtpVerifed: {
+        type: Boolean,
+        default: false
+    },
 
-},{timestamps:true})
 
-const User = mongoose.model("User",userSchema)
+}, { timestamps: true })
+
+const User = mongoose.model("User", userSchema)
 
 export default User;
