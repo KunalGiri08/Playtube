926054f3a122f5001f029dff16c2919841f0226e
Kunal Giri <kunalgiri177@gmail.com>
2026-07-27 19:08:56 +0530

    feat: implement OTP-based password reset functionality with email verification

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -3,6 +3,7 @@ import User from "../model/userModel.js"
 import validator from "validator"
 import bcrypt from "bcryptjs"
 import genToken from "../config/token.js";
+import sendMail from "../config/sendMail.js";
 
 export const signUp = async (req, res) => {
     try {
@@ -137,3 +138,65 @@ export const googleAuth = async (req, res) => {
     return res.status(500).json({ message: `GoogleAuth error ${error}` });
   }
 };
+
+export const sendOTP = async (req,res) => {
+    try {
+        const {email} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+             return res.status(404).json({message:"User not found"})
+        }
+        const otp = Math.floor(1000 + Math.random()* 9000).toString()
+        
+        user.resetOtp = otp,
+        user.otpExpires = Date.now() + 5 * 60 *1000,
+        user.isOtpVerifed = false
+
+        await user.save()
+        await sendMail(email , otp)
+        return res.status(200).json({message:"Otp Send Successfully"})
+    } catch (error) {
+        return res.status(500).json({message:`send Otp error ${error}`})
+    }
+    
+}
+
+export const verifyOTP = async (req,res) => {
+    try {
+        const {email,otp} = req.body
+         const user = await User.findOne({email})
+        if(!user || user.resetOtp != otp || user.otpExpires < Date.now() ){
+             return res.status(404).json({message:"Invalid OTP"})
+        }
+        user.isOtpVerifed = true,
+        user.resetOtp = undefined,
+        user.otpExpires = undefined
+
+        await user.save()
+
+         return res.status(200).json({message:"Otp Verified Successfully"})
+
+    } catch (error) {
+         return res.status(500).json({message:`verify Otp error ${error}`})
+    }
+}
+
+
+export const resetPassword = async (req,res) => {
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(!user || !user.isOtpVerifed){
+             return res.status(404).json({message:"OTP verification is required"})
+        }
+        const hashPassword = await bcrypt.hash(password,10)
+        user.password = hashPassword,
+        user.isOtpVerifed = false
+
+        await user.save()
+         return res.status(200).json({message:"Reset Password Successfully"})
+
+    } catch (error) {
+        return res.status(500).json({message:`reset password error ${error}`})
+    }
+}
--- backend/route/authRoute.js
+++ backend/route/authRoute.js
@@ -1,11 +1,14 @@
 import express from "express"
 import upload from "../middleware/multer.js"
-import { signIn, signOut, signUp } from "../controller/authController.js"
+import { resetPassword, sendOTP, signIn, signOut, signUp, verifyOTP } from "../controller/authController.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup", upload.single("photoUrl"), signUp)
 authRouter.post("/signin", signIn)
 authRouter.post("/signout", signOut)
+authRouter.post("/sendotp",sendOTP)
+authRouter.post("/verifyotp",verifyOTP)
+authRouter.post("/resetpassword",resetPassword)
 
 export default authRouter
