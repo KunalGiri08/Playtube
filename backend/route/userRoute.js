1daca9dfd5ed31892f1ce8598c4e940b81343e39
Kunal Giri <kunalgiri177@gmail.com>
2026-07-29 23:14:47 +0530

    feat: add create-channel route with file upload support

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
-import { getCurrentUser } from "../controller/userController.js";
+import { createChannel, getCurrentUser } from "../controller/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/getcurrentuser", isAuth, getCurrentUser);
+
+userRouter.post("/create-channel", isAuth , upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "bannerImage", maxCount: 1 }
+  ]) , createChannel)
 
 export default userRouter;
