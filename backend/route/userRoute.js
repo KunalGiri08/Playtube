db3f7e55601cb8f9298826d90dde093db1b1a31b
Kunal Giri <kunalgiri177@gmail.com>
2026-07-29 23:15:21 +0530

    feat: import multer middleware for file upload handling in user routes

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
 import { createChannel, getCurrentUser } from "../controller/userController.js";
+import upload from "../middleware/multer.js";
 
 const userRouter = express.Router();
 
