6b3c0b33719db0c1cd7773ee33b0b81fabd8ba13
Kunal Giri <kunalgiri177@gmail.com>
2026-07-30 18:58:03 +0530

    feat: add setChannelData reducer to manage channel data in user slice

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -4,15 +4,20 @@ const userSlice = createSlice({
     name: "user",
 
     initialState: {
-        userData: null
+        userData: null,
+        channelData:null,
     },
 
     reducers: {
         setUserData: (state, action) => {
             state.userData = action.payload;
-        }
+        },
+         setChannelData:(state,action)=>{
+            state.channelData = action.payload
+        },
     }
 });
 
 export const { setUserData } = userSlice.actions;
+export const {setChannelData} = userSlice.actions
 export default userSlice.reducer;
