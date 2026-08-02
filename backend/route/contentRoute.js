4bc35502827a7a22356438887f9a00b720cc7a33
Kunal Giri <kunalgiri177@gmail.com>
2026-08-02 12:40:09 +0530

    feat: refactor content routes for improved readability and organization

--- backend/route/contentRoute.js
+++ backend/route/contentRoute.js
@@ -1,22 +1,24 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js";
 import upload from "../middleware/multer.js";
-import { createVideo } from "../controller/videoController.js";
-import { createShort } from "../controller/shortController.js";
+import { createVideo, getChannelVideos } from "../controller/videoController.js";
+import { createShort, getAllShorts } from "../controller/shortController.js";
 
 const contentRouter = express.Router()
-  
-// for videoController
-contentRouter.post("/upload-video",isAuth,upload.fields([
-    { name: "video", maxCount: 1 },
-    { name: "thumbnail", maxCount: 1 }
-  ]),createVideo);
 
+// for videoController
+contentRouter.post("/upload-video", isAuth, upload.fields([
+  { name: "video", maxCount: 1 },
+  { name: "thumbnail", maxCount: 1 }
+]), createVideo);
+
+contentRouter.post("/get-videos", isAuth, getChannelVideos);
 
 
 //for shortController
-contentRouter.post("/upload-short",isAuth,upload.single("short") , createShort)
+contentRouter.post("/upload-short", isAuth, upload.single("short"), createShort)
+contentRouter.get("/allshorts", getAllShorts)
 
 
 
-  export default contentRouter
+export default contentRouter
