c7fbd4137e6e7b75aa4d6993ec28fab287ce9980
Kunal Giri <kunalgiri177@gmail.com>
2026-08-06 21:39:07 +0530

    feat: add addComment and addReply routes for video comments management

--- backend/route/contentRoute.js
+++ backend/route/contentRoute.js
@@ -1,7 +1,7 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js";
 import upload from "../middleware/multer.js";
-import { addView, createVideo, getChannelVideos, toggleDislikeVideo, toggleLikeVideo, toggleSaveVideo } from "../controller/videoController.js";
+import { addComment, addReply, addView, createVideo, getChannelVideos, toggleDislikeVideo, toggleLikeVideo, toggleSaveVideo } from "../controller/videoController.js";
 import { createShort, getAllShorts } from "../controller/shortController.js";
 
 const contentRouter = express.Router()
@@ -23,6 +23,12 @@ contentRouter.put("/video/:videoId/toggle-dislike", isAuth, toggleDislikeVideo);
 contentRouter.put("/video/:videoId/toggle-save", isAuth, toggleSaveVideo);
 // 👁️ Add view
 contentRouter.put("/video/:videoId/add-view", addView);
+
+// 💬 Add comment
+contentRouter.post("/video/:videoId/comment", isAuth, addComment);
+
+// 💬 Add reply to comment
+contentRouter.post("/video/:videoId/:commentId/reply", isAuth, addReply);
 
 
 //for shortController
