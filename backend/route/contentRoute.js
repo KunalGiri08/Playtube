67852320edde51a3ea0891063398fa89ae8c6cb5
Kunal Giri <kunalgiri177@gmail.com>
2026-08-09 21:19:32 +0530

    feat: add routes for managing shorts including views, likes, dislikes, saves, comments, and replies

--- backend/route/contentRoute.js
+++ backend/route/contentRoute.js
@@ -2,7 +2,7 @@ import express from "express"
 import isAuth from "../middleware/isAuth.js";
 import upload from "../middleware/multer.js";
 import { addComment, addReply, addView, createVideo, getChannelVideos, toggleDislikeVideo, toggleLikeVideo, toggleSaveVideo } from "../controller/videoController.js";
-import { createShort, getAllShorts } from "../controller/shortController.js";
+import { addCommentforShort, addReplyforShort, addViewforShort, createShort, getAllShorts, toggleDislikeShort, toggleLikeShort, toggleSaveShort } from "../controller/shortController.js";
 
 const contentRouter = express.Router()
 
@@ -34,6 +34,26 @@ contentRouter.post("/video/:videoId/:commentId/reply", isAuth, addReply);
 //for shortController
 contentRouter.post("/upload-short", isAuth, upload.single("short"), createShort)
 contentRouter.get("/allshorts", getAllShorts)
+
+// 👁️ Add view
+contentRouter.put("/short/:shortId/add-view",isAuth, addViewforShort);
+
+// 👍 Like short
+contentRouter.put("/short/:shortId/toggle-like", isAuth, toggleLikeShort);
+
+// 👎 Dislike short
+contentRouter.put("/short/:shortId/toggle-dislike", isAuth, toggleDislikeShort);
+
+// 💾 Save / Unsave short
+contentRouter.put("/short/:shortId/toggle-save", isAuth, toggleSaveShort);
+
+// 💬 Add comment short
+contentRouter.post("/short/:shortId/comment", isAuth, addCommentforShort);
+
+// 💬 Add reply to comment short
+contentRouter.post("/short/:shortId/:commentId/reply", isAuth, addReplyforShort);
+
+
 
 
 
