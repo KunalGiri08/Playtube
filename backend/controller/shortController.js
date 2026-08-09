d66240d19be1f253fa347b35e82c28c370e38187
Kunal Giri <kunalgiri177@gmail.com>
2026-08-09 21:14:55 +0530

    feat: add functions for liking, disliking, commenting, replying, viewing, and saving shorts

--- backend/controller/shortController.js
+++ backend/controller/shortController.js
@@ -54,3 +54,163 @@ export const getAllShorts = async (req, res) => {
 res.status(500).json({message : "Failed to fetch short"});
   }
 };
+
+
+
+// ---------------- LIKE VIDEO ----------------
+export const toggleLikeShort = async (req, res) => {
+  try {
+    const { shortId } = req.params;
+    const userId = req.userId; // ✅ auth middleware se aayega
+
+    const short = await Short.findById(shortId);
+    if (!short) return res.status(404).json({ message: "short not found" });
+
+    if (short?.likes.includes(userId)) {
+      // already liked → remove
+      short.likes.pull(userId);
+    } else {
+      // add like → remove dislike if exists
+      short?.likes.push(userId);
+      short?.dislikes.pull(userId);
+    }
+
+    await short.save();
+     await short.populate("comments.author", "username photoUrl");
+    await short.populate("channel")
+     await short.populate("comments.replies.author", "username photoUrl");
+   return res.status(200).json(short);
+  } catch (error) {
+  return  res.status(500).json({ message: "Error toggling like", error: error.message });
+  }
+};
+
+// ---------------- DISLIKE VIDEO ----------------
+export const toggleDislikeShort = async (req, res) => {
+  try {
+    const { shortId } = req.params;
+    const userId = req.userId;
+
+    const short = await Short.findById(shortId);
+    if (!short) return res.status(404).json({ message: "short not found" });
+
+    if (short.dislikes.includes(userId)) {
+      short.dislikes.pull(userId);
+    } else {
+      short.dislikes.push(userId);
+      short.likes.pull(userId);
+    }
+ await short.populate("comments.author", "username photoUrl");
+    await short.populate("channel")
+     await short.populate("comments.replies.author", "username photoUrl");
+    await short.save();
+    return res.status(200).json(short);
+  } catch (error) {
+    return res.status(500).json({ message: "Error toggling dislike", error: error.message });
+  }
+};
+
+// ---------------- ADD COMMENT ----------------
+// ---------------- ADD COMMENT ----------------
+export const addCommentforShort = async (req, res) => {
+  try {
+    const { shortId } = req.params;
+    const { message } = req.body;
+    const userId = req.userId;
+
+    // pehle video lao
+    const short = await Short.findById(shortId);
+    if (!short) return res.status(404).json({ message: "short not found" });
+
+    // comment TOP pe add karo
+    short.comments.unshift({ author: userId, message });
+    await short.save();
+ await short.populate("comments.author", "username photoUrl");
+    await short.populate("channel")
+     await short.populate("comments.replies.author", "username photoUrl");
+
+    return res.status(201).json(short);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error adding comment", error: error.message });
+  }
+};
+
+// ---------------- ADD REPLY ----------------
+export const addReplyforShort = async (req, res) => {
+  try {
+    const { shortId, commentId } = req.params;
+    const { message } = req.body;
+    const userId = req.userId;
+
+    const short = await Short.findById(shortId);
+    if (!short) return res.status(404).json({ message: "Video not found" });
+
+    const comment = short.comments.id(commentId);
+    if (!comment) return res.status(404).json({ message: "Comment not found" });
+
+    // reply TOP pe add karo
+    comment.replies.unshift({ author: userId, message });
+    await short.save();
+      await short.populate("comments.author", "username photoUrl");
+    await short.populate("channel")
+     await short.populate("comments.replies.author", "username photoUrl");
+
+    return res.status(201).json(short);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error adding reply", error: error.message });
+  }
+};
+
+
+// ---------------- ADD VIEW ----------------
+export const addViewforShort = async (req, res) => {
+  try {
+    const { shortId } = req.params;
+
+    const short = await Short.findByIdAndUpdate(
+      shortId,
+      { $inc: { views: 1 } },
+      { new: true }
+    );
+
+    if (!short) return res.status(404).json({ message: "Video not found" });
+
+      await short.populate("comments.author", "username photoUrl");
+    await short.populate("channel")
+     await short.populate("comments.replies.author", "username photoUrl");
+
+   return res.status(200).json(short);
+  } catch (error) {
+   return res.status(500).json({ message: "Error adding view", error: error.message });
+  }
+};
+
+// ---------------- TOGGLE SAVE ----------------
+export const toggleSaveShort = async (req, res) => {
+  try {
+    const { shortId } = req.params;
+    const userId = req.userId;
+
+    const short = await Short.findById(shortId);
+    if (!short) return res.status(404).json({ message: "Video not found" });
+
+    if (short.saveBy.includes(userId)) {
+      short.saveBy.pull(userId);
+    } else {
+      short.saveBy.push(userId);
+    }
+    await short.populate("comments.author", "username photoUrl");
+    await short.populate("channel")
+     await short.populate("comments.replies.author", "username photoUrl");
+
+    await short.save();
+   return res.status(200).json(short);
+  } catch (error) {
+   return res.status(500).json({ message: "Error toggling save", error: error.message });
+  }
+};
+
