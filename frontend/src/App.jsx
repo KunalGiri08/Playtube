e25c9e408bb6653b28573e65e59a82178328fc7e
KunalGiri08 <kunalgiri177@gmail.com>
2026-08-21 16:35:26 +0530

    feat: enhance routing by adding new protected routes for video, post, and playlist creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Shorts from "./pages/Shorts/Shorts";
-import SignIn from './Pages/SignIn'
-import SignUp from './Pages/SignUp'
-import CustomAlert from './component/CustomAlert'
-import getCurrentUser from './customHooks/GetCurrentUser';
-import UsegetChannel from './customHooks/GetChannelData';
-import ForgetPassword from './Pages/ForgetPassword';
-import MobileProfile from './component/MobileProfile';
-import CreateChannel from './Pages/Channel/CreateChannel';
-import ViewChannel from './Pages/Channel/ViewChannel';
-import UpdateChannel from './Pages/Channel/UpdateChannel';
-import { useSelector } from 'react-redux';
-export const serverUrl = "http://localhost:8000"
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import CustomAlert from "./component/CustomAlert";
+import getCurrentUser from "./customHooks/UsegetCurrentUser";
+import UsegetChannel from "./customHooks/GetChannelData";
+import ForgetPassword from "./Pages/ForgetPassword";
+import MobileProfile from "./component/MobileProfile";
+import CreateChannel from "./Pages/Channel/CreateChannel";
+import ViewChannel from "./Pages/Channel/ViewChannel";
+import UpdateChannel from "./Pages/Channel/UpdateChannel";
+import CreateVideo from "./Pages/Videos/createVideo";
+import CreatePost from "./Pages/Post/createPost";
+import CreateShorts from "./Pages/Shorts/createShorts";
+import CreatePlaylist from "./Pages/Playlist/createPlaylist";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import GetAllContentData from "./customHooks/UsegetAllContentData";
+import WatchShortPage from "./Pages/Shorts/WatchShortPage";
+export const serverUrl = "http://localhost:8000";
 
 const ProtectedRoute = ({ userData, children }) => {
   if (!userData) {
@@ -24,18 +31,80 @@ const ProtectedRoute = ({ userData, children }) => {
 };
 
 function App() {
-  getCurrentUser()
-  UsegetChannel()
-
-  const { userData } = useSelector(state => state.user)
+  getCurrentUser();
+  UsegetChannel();
+  GetAllContentData();
+  const { userData } = useSelector((state) => state.user);
   return (
     <>
       <CustomAlert />
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path='/shorts' element={<ProtectedRoute userData={userData}><Shorts /></ProtectedRoute>} />
-          <Route path='/viewchannel' element={<ProtectedRoute userData={userData}><ViewChannel /></ProtectedRoute>} />
-          <Route path='/updatechannel' element={<ProtectedRoute userData={userData}><UpdateChannel /></ProtectedRoute>} />
+          <Route
+            path="/shorts"
+            element={
+              <ProtectedRoute userData={userData}>
+                <Shorts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/viewchannel"
+            element={
+              <ProtectedRoute userData={userData}>
+                <ViewChannel />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/updatechannel"
+            element={
+              <ProtectedRoute userData={userData}>
+                <UpdateChannel />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-video"
+            element={
+              <ProtectedRoute userData={userData}>
+                <CreateVideo />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-post"
+            element={
+              <ProtectedRoute userData={userData}>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-short"
+            element={
+              <ProtectedRoute userData={userData}>
+                <CreateShorts />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/watch-short/:shortId"
+            element={
+              <ProtectedRoute userData={userData}>
+                <WatchShortPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/create-playlist"
+            element={
+              <ProtectedRoute userData={userData}>
+                <CreatePlaylist />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/mobilepro" element={<MobileProfile />} />
         </Route>
 
@@ -43,10 +112,9 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgetpassword" element={<ForgetPassword />} />
         <Route path="/createchannel" element={<CreateChannel />} />
-
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
