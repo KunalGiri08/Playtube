08a50e615c6c3fa1572021091475c6bd378b369f
KunalGiri08 <kunalgiri177@gmail.com>
2026-08-26 22:28:54 +0530

    refactor: improve formatting and consistency in user model schema

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,41 +1,60 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
+      type: String,
     },
     photoUrl: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     channel: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Channel"
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
     },
-     // 🔐 OTP reset system
-  resetOtp: {
-        type: String
+    // 🔐 OTP reset system
+    resetOtp: {
+      type: String,
     },
     otpExpires: {
-        type: Date
+      type: Date,
     },
     isOtpVerifed: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
+    // 📌 History Field
+    history: [
+      {
+        contentId: {
+          type: mongoose.Schema.Types.ObjectId,
+          refPath: "history.contentType", // dynamically decide karega Video ya Short
+        },
+        contentType: {
+          type: String,
+          enum: ["Video", "Short"],
+          required: true,
+        },
+        watchedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
-
-}, { timestamps: true })
-
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
